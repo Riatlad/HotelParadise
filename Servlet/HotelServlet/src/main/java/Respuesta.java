1cbd351b2acad7fa0db52675fdec7987281cc938
Daniel Ramón Morales Perich <daninanaya@gmail.com>
2024-01-15 21:28:46 +0100

    My job here (y más) is done.

---
+++ Servlet/HotelServlet/src/main/java/Respuesta.java
@@ -0,0 +1,29 @@
+import com.google.gson.Gson;
+
+import java.util.List;
+
+public class Respuesta {
+
+    public String clientesJSON(List<Cliente> clientes) {
+        StringBuilder clientesJSON = new StringBuilder();
+        Gson gson = new Gson();
+
+        for (int i = 0; i < clientes.size(); i++) {
+            clientesJSON.append(gson.toJson(clientes.get(i)));
+        }
+
+        System.out.println(clientesJSON);
+        return clientesJSON.toString();
+    }
+
+    public String habitacionesJSON(List<Habitacion> habitaciones) {
+        StringBuilder habitacionesJSON = new StringBuilder();
+        Gson gson = new Gson();
+
+        for (int i = 0; i < habitaciones.size(); i++) {
+            habitacionesJSON.append(gson.toJson(habitaciones.get(i)));
+        }
+
+        return habitacionesJSON.toString();
+    }
+}
