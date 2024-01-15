39947283c71ec716fc4bf3650846a17dced92c75
Adán <adanhiguerasmendozar@gmail.com>
2024-01-15 23:29:58 +0100

    Mi parte del proyecto, Creacion de clases basicas

---
+++ Proyecto/HotelParadise/app/src/main/java/com/HotelParadise/App/Model/Cliente.java
@@ -0,0 +1,44 @@
+package com.HotelParadise.App.Model;
+
+public class Cliente {
+    private String dni;
+    private String nombre;
+    private String apellidos;
+    private String email;
+    private String telefono;
+    private String numTarjeta;
+
+    public Cliente(String dni, String nombre, String apellidos, String email, String telefono, String numTarjeta) {
+        this.dni = dni;
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.email = email;
+        this.telefono = telefono;
+        this.numTarjeta = numTarjeta;
+    }
+
+
+    public String getDni() {
+        return dni;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public String getNumTarjeta() {
+        return numTarjeta;
+    }
+}
