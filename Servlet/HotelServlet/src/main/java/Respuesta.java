cd1e31014093e909356e6896cf0d2eed1efb0acf
Daniel Ramón Morales Perich <daninanaya@gmail.com>
2024-01-21 21:25:31 +0100

    Ajustes a respuesta y añadido RespuestaTest

--- Servlet/HotelServlet/src/main/java/Respuesta.java
+++ Servlet/HotelServlet/src/main/java/Respuesta.java
@@ -3,22 +3,22 @@ import com.google.gson.Gson;
 import java.util.List;
 
 public class Respuesta {
+    private Gson gson;
 
     public String clientesJSON(List<Cliente> clientes) {
         StringBuilder clientesJSON = new StringBuilder();
-        Gson gson = new Gson();
+        gson = new Gson();
 
         for (int i = 0; i < clientes.size(); i++) {
             clientesJSON.append(gson.toJson(clientes.get(i)));
         }
 
-        System.out.println(clientesJSON);
         return clientesJSON.toString();
     }
 
     public String habitacionesJSON(List<Habitacion> habitaciones) {
         StringBuilder habitacionesJSON = new StringBuilder();
-        Gson gson = new Gson();
+        gson = new Gson();
 
         for (int i = 0; i < habitaciones.size(); i++) {
             habitacionesJSON.append(gson.toJson(habitaciones.get(i)));
